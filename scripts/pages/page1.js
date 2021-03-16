595c3fcf9532a08cddb9e61ad544d6d9e044cd45
hasretsariyer <hasretsariyer@gmail.com>
2021-03-16 15:52:20 +0300

    Update page1.js

--- scripts/pages/page1.js
+++ scripts/pages/page1.js
@@ -1,51 +1,96 @@
-const touch = require("sf-extension-utils/lib/touch");
-const componentContextPatch = require("@smartface/contx/lib/smartface/componentContextPatch");
-const PageTitleLayout = require("components/PageTitleLayout");
-
+const Page = require("sf-core/ui/page");
 const extend = require("js-base/core/extend");
-const System = require("sf-core/device/system");
+const Color = require('sf-core/ui/color');
+const Label = require('sf-core/ui/label');
+const FlexLayout = require('sf-core/ui/flexlayout');
+const ListView = require('sf-core/ui/listview');
+const ListViewItem = require('sf-core/ui/listviewitem');
+const TextAlignment = require('sf-core/ui/textalignment');
+const Font = require('sf-core/ui/font');
 
-// Get generated UI code
-const Page1Design = require("ui/ui_page1");
+var Page1 = extend(Page)(
+    function(_super) {
+        _super(this, {
+            onShow: function(params) {
+                this.headerBar.visible = false;
+            }
+        });
+        this.layout.flexDirection = FlexLayout.FlexDirection.ROW;
+        this.layout.justifyContent = FlexLayout.JustifyContent.CENTER;
+        this.layout.alignItems = FlexLayout.AlignItems.CENTER;
 
-const Page1 = extend(Page1Design)(
-    // Constructor
-    function(_super, routeData, router) {
-        // Initalizes super class for this page scope
-        _super(this);
-        // Overrides super.onShow method
-        this.onShow = onShow.bind(this, this.onShow.bind(this));
-        // Overrides super.onLoad method
-        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
-        this.btnNext.onPress = () => {
-            this.router.push("/pages/page2", { message: "Hello World!" });
+        var myDataSet = [{
+            title: 'Smartface Title 1',
+            backgroundColor: Color.create("#99d9f9")
+        }, {
+            title: 'Smartface Title 2',
+            backgroundColor: Color.create("#66c6f6")
+        }, {
+            title: 'Smartface Title 3',
+            backgroundColor: Color.create("#32b3f3")
+        }, {
+            title: 'Smartface Title 4',
+            backgroundColor:Color.create("#00a1f1")
+        }, {
+            title: 'Smartface Title 5',
+            backgroundColor:Color.create("#00a1f1")
+        }, {
+            title: 'Smartface Title 6',
+            backgroundColor:Color.create("#00a1f1")
+        }, {
+            title: 'Smartface Title 7',
+            backgroundColor:Color.create("#00a1f1")
+        }, {
+            title: 'Smartface Title 8',
+            backgroundColor:Color.create("#00a1f1")
+        }, {
+            title: 'Smartface Title 9',
+            backgroundColor:Color.create("#00a1f1")
+        }];
+
+        var myListView = new ListView({
+            height:350,
+            rowHeight: 70,
+            itemCount: myDataSet.length,
+        });
+
+        this.layout.addChild(myListView);
+            
+        myListView.onRowCreate = function() {
+            var myListViewItem = new ListViewItem();
+            var myLabelTitle = new Label({
+                flexGrow:1,
+                marginTop: 10,
+                marginBottom: 10,
+                marginLeft: 50,
+                marginRight: 50
+            });
+            myLabelTitle.font = Font.create(Font.DEFAULT, 15, Font.BOLD);
+            myLabelTitle.textAlignment = TextAlignment.MIDCENTER;
+            myLabelTitle.textColor = Color.WHITE;
+            myLabelTitle.borderRadius = 10;
+            myListViewItem.addChild(myLabelTitle);
+            myListViewItem.myLabelTitle = myLabelTitle;
+            return myListViewItem;
         };
-    });
+        myListView.onRowBind = function(listViewItem, index) {
+            var myLabelTitle = listViewItem.myLabelTitle;
+            myLabelTitle.text = myDataSet[index].title;
+            myLabelTitle.backgroundColor = myDataSet[index].backgroundColor;
+        };
+        myListView.onRowSelected = function(listViewItem, index) {
+            console.log("selected index = " + index)
+        };
 
-/**
- * @event onShow
- * This event is called when a page appears on the screen (everytime).
- * @param {function} superOnShow super onShow function
- * @param {Object} parameters passed from Router.go function
- */
-function onShow(superOnShow) {
-    superOnShow();
-    this.headerBar.titleLayout.applyLayout();
-}
-/**
- * 
- * @event onLoad
- * This event is called once when page is created.
- * @param {function} superOnLoad super onLoad function
- */
-function onLoad(superOnLoad) {
-    superOnLoad();
-    this.headerBar.leftItemEnabled = false;
-    this.headerBar.titleLayout = new PageTitleLayout();
-    componentContextPatch(this.headerBar.titleLayout, "titleLayout");
-    if (System.OS === "Android") {
-        this.headerBar.title = "";
+        myListView.onPullRefresh = function() {
+            myDataSet.push({
+                title: 'Smartface Title ' + (myDataSet.length+1),
+                backgroundColor: Color.RED,
+            })
+            myListView.itemCount = myDataSet.length;
+            myListView.refreshData();
+            myListView.stopRefresh();
+        }
     }
-}
-
+);
 module.exports = Page1;
